d2d3a015a2328d38fc472c374d3db0d93c33a85c
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-10-06 22:48:34 +0800

    Add missing import

--- src/Entity/FloorArea.php
+++ src/Entity/FloorArea.php
@@ -7,6 +7,7 @@ use JsonSerializable;
 use App\Repository\FloorAreaRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=FloorAreaRepository::class)
